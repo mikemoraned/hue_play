0973d81ea151788f12b2e8af26806515f7f14c03
Mike Moran <mike@houseofmoran.com>
2020-10-18 00:43:39 +0100

    load all lights

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,61 @@
 extern crate hueclient;
+use dotenv;
 use hueclient::bridge::Bridge;
-
-#[allow(dead_code)]
 fn main() {
-    let bridge = Bridge::discover().unwrap();
+    let username = dotenv::var("USERNAME").unwrap();
+    println!("Loaded username");
+    let bridge = Bridge::discover().unwrap().with_user(username);
     println!("Hue bridge found: {:?}", bridge);
+
+    match bridge.get_all_lights() {
+        Ok(lights) => {
+            println!("id name                 on    bri   hue sat temp  x      y");
+            for ref l in lights.iter() {
+                println!(
+                    "{:2} {:20} {:5} {:3} {:5} {:3} {:4}K {:4} {:4}",
+                    l.id,
+                    l.light.name,
+                    if l.light.state.on { "on" } else { "off" },
+                    if l.light.state.bri.is_some() {
+                        l.light.state.bri.unwrap()
+                    } else {
+                        0
+                    },
+                    if l.light.state.hue.is_some() {
+                        l.light.state.hue.unwrap()
+                    } else {
+                        0
+                    },
+                    if l.light.state.sat.is_some() {
+                        l.light.state.sat.unwrap()
+                    } else {
+                        0
+                    },
+                    if l.light.state.ct.is_some() {
+                        l.light
+                            .state
+                            .ct
+                            .map(|k| if k != 0 { 1000000u32 / (k as u32) } else { 0 })
+                            .unwrap()
+                    } else {
+                        0
+                    },
+                    if l.light.state.xy.is_some() {
+                        l.light.state.xy.unwrap().0
+                    } else {
+                        0.0
+                    },
+                    if l.light.state.xy.is_some() {
+                        l.light.state.xy.unwrap().1
+                    } else {
+                        0.0
+                    },
+                );
+            }
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+            ::std::process::exit(2)
+        }
+    }
 }
